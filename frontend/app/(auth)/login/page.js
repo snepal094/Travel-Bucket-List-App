8470cfa1860bd43ac8ebf3a1988d38b9386c10db
Suyasha Nepal <nepalsuyasha@gmail.com>
2025-06-17 15:18:33 +0545

    login & register UI

--- frontend/app/(auth)/login/page.js
+++ frontend/app/(auth)/login/page.js
@@ -1,24 +1,47 @@
 'use client';
 
-import React, { use } from 'react';
+import React, { useState } from 'react';
 import { Formik } from 'formik';
 import {
   Box,
   Button,
   FormControl,
   FormHelperText,
+  IconButton,
+  InputAdornment,
+  InputLabel,
   LinearProgress,
+  OutlinedInput,
+  Snackbar,
   TextField,
 } from '@mui/material';
 import { loginValidationSchema } from '@/validation-schema/login.validation.schema';
-import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import $axios from '@/lib/axios/axios.instance';
 import { useMutation } from '@tanstack/react-query';
+import Slideshow from '@/components/SlideShow';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
 
 const Login = () => {
   const router = useRouter();
+
+  //* password
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const handleMouseUpPassword = (event) => {
+    event.preventDefault();
+  };
+
+  //* snackbar
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState('');
 
   const { isPending, error, mutate } = useMutation({
     mutationKey: ['login-user'],
@@ -26,7 +49,8 @@ const Login = () => {
       return await $axios.post('/user/login', values);
     },
     onSuccess: (response) => {
-      console.log(response);
+      // response.data has API response
+      // console.log(response);
       window.localStorage.setItem('token', response?.data?.accessToken);
       window.localStorage.setItem(
         'firstName',
@@ -37,16 +61,29 @@ const Login = () => {
         response?.data?.userDetails?.currentRole
       );
 
-      router.push('/');
+      setSnackbarMessage(response.data.message);
+      setSnackbarOpen(true);
+      setTimeout(() => {
+        router.push('/');
+      }, 1000);
     },
     onError: (error) => {
+      //error.message = generic error string (from JS/axios/browser)
+      //error.response.data.message = custom message sent by your backend API (if available)
+      // console.log(error);
       console.log(error.response.data.message);
     },
   });
 
   return (
-    <Box>
+    <Box
+      sx={{
+        display: 'flex',
+        gap: '8rem',
+      }}
+    >
       {isPending && <LinearProgress color="secondary" />}
+      <Slideshow />
       <Formik
         initialValues={{
           identifier: '',
@@ -89,54 +126,97 @@ const Login = () => {
       >
         {(formik) => {
           return (
-            <form
-              onSubmit={formik.handleSubmit}
-              className="flex flex-col justify-between min-w-[400px] items-center shadow-2xl shadow-gray-500 px-8 py-4 min-h-[400px]"
-            >
-              <p className="text-3xl font-bold">Log In</p>
-
-              <FormControl fullWidth>
-                <TextField
-                  label="Email or Username"
-                  {...formik.getFieldProps('identifier')}
-                />
-                {formik.touched.identifier && formik.errors.identifier ? (
-                  <FormHelperText error>
-                    {formik.errors.identifier}
-                  </FormHelperText>
-                ) : null}
-              </FormControl>
-
-              <FormControl fullWidth>
-                <TextField
-                  label="Password"
-                  {...formik.getFieldProps('password')}
-                />
-                {formik.touched.password && formik.errors.password ? (
-                  <FormHelperText error>
-                    {formik.errors.password}
-                  </FormHelperText>
-                ) : null}
-              </FormControl>
-
-              {/* FormControl is a div */}
-              <Button
-                fullWidth
-                type="submit"
-                variant="contained"
-                color="secondary"
-                disabled={isPending}
-              >
-                Log In
-              </Button>
-              {/*className doesn't work in Button*/}
-
-              <div>
-                <Link href={'/register'} className="text-blue-700 underline">
-                  New here? Register
-                </Link>
+            <div>
+              <div className="flex flex-col items-center">
+                <p className="text-4xl font-extrabold text-[#7B8D6A] mb-2">
+                  BucketMap
+                </p>
+                <p className="text-sm text-gray-600 mb-6">
+                  Explore. Mark. Remember.
+                </p>
               </div>
-            </form>
+              <form
+                onSubmit={formik.handleSubmit}
+                className="flex flex-col justify-between min-w-[400px] items-center shadow-2xl shadow-gray-500 px-8 pt-4 pb-6 min-h-[600px]"
+              >
+                <p className="text-4xl font-bold mt-4">Log In</p>
+
+                <div className="flex flex-col gap-8 w-full">
+                  <FormControl fullWidth>
+                    {/* FormControl is a div */}
+                    <TextField
+                      label="Email or Username"
+                      {...formik.getFieldProps('identifier')}
+                    />
+                    {formik.touched.identifier && formik.errors.identifier ? (
+                      <FormHelperText error>
+                        {formik.errors.identifier}
+                      </FormHelperText>
+                    ) : null}
+                  </FormControl>
+
+                  <FormControl fullWidth variant="outlined">
+                    <InputLabel htmlFor="outlined-adornment-password">
+                      Password
+                    </InputLabel>
+
+                    <OutlinedInput
+                      {...formik.getFieldProps('password')}
+                      // type={showPassword ? 'text' : 'password'}
+                      type={showPassword ? 'text' : 'password'}
+                      endAdornment={
+                        <InputAdornment position="end">
+                          <IconButton
+                            aria-label={
+                              showPassword
+                                ? 'hide the password'
+                                : 'display the password'
+                            }
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
+                            onMouseUp={handleMouseUpPassword}
+                            edge="end"
+                          >
+                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                          </IconButton>
+                        </InputAdornment>
+                      }
+                      label="Password"
+                    />
+
+                    {formik.touched.password && formik.errors.password ? (
+                      <FormHelperText error>
+                        {formik.errors.password}
+                      </FormHelperText>
+                    ) : null}
+                  </FormControl>
+                </div>
+
+                <Button
+                  fullWidth
+                  type="submit"
+                  variant="contained"
+                  // color="secondary"
+                  sx={{ backgroundColor: '#7B8D6A', color: '#fff' }}
+                  disabled={isPending}
+                >
+                  Log In
+                </Button>
+                {/*className doesn't work in Button*/}
+                <span>
+                  Don't have an account?{' '}
+                  <Link href={'/register'} className="text-blue-700 font-bold">
+                    Sign up
+                  </Link>
+                </span>
+              </form>
+              <Snackbar
+                open={snackbarOpen}
+                autoHideDuration={6000}
+                message={snackbarMessage}
+                onClose={() => setSnackbarOpen(false)}
+              />
+            </div>
           );
         }}
       </Formik>
--- frontend/app/(auth)/register/page.js
+++ frontend/app/(auth)/register/page.js
@@ -3,41 +3,81 @@
 // because Formik only works in the browser
 // since it depends on window and handling form input events
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Formik } from 'formik';
 import {
   Box,
   Button,
   FormControl,
+  FormControlLabel,
   FormHelperText,
+  FormLabel,
+  IconButton,
+  InputAdornment,
+  InputLabel,
   LinearProgress,
+  MenuItem,
+  OutlinedInput,
+  Radio,
+  RadioGroup,
+  Select,
+  Snackbar,
+  SnackbarContent,
   TextField,
+  ToggleButton,
+  ToggleButtonGroup,
 } from '@mui/material';
 import { registerValidationSchema } from '@/validation-schema/register.validation.schema';
-import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import { useMutation } from '@tanstack/react-query';
 import $axios from '@/lib/axios/axios.instance';
+import genders from '@/constants/genders';
+import countries from '@/constants/countries';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
 
 const Register = () => {
   const router = useRouter();
+
+  //* password
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const handleMouseUpPassword = (event) => {
+    event.preventDefault();
+  };
+
+  //* snackbar
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState('');
 
   const { isPending, error, mutate } = useMutation({
     mutationKey: ['register-user'], //unique name
     mutationFn: async (values) => {
       return await $axios.post('/user/register', values);
     },
-    onSuccess: () => {
-      router.push('/login');
+    onSuccess: (response) => {
+      // console.log(response);
+      setSnackbarMessage(response.data.message);
+      setSnackbarOpen(true);
+      setTimeout(() => {
+        router.push('/login');
+      }, 1000);
     },
     onError: (error) => {
-      console.log(error.response.data.message);
+      setSnackbarMessage(error.response.data.message);
+      setSnackbarOpen(true);
     },
   });
   return (
     <Box>
       {isPending && <LinearProgress color="secondary" />}
+
       <Formik
         initialValues={{
           email: '',
@@ -69,112 +109,254 @@ const Register = () => {
       >
         {(formik) => {
           return (
-            <form
-              onSubmit={formik.handleSubmit}
-              className="flex flex-col justify-between min-w-[400px] items-center shadow-2xl shadow-gray-500 px-8 py-4 min-h-[400px] gap-3"
-            >
-              <p className="text-3xl font-bold">Register</p>
-
-              <FormControl fullWidth>
-                <TextField label="Email" {...formik.getFieldProps('email')} />
-                {formik.touched.email && formik.errors.email ? (
-                  <FormHelperText error>{formik.errors.email}</FormHelperText>
-                ) : null}
-              </FormControl>
-
-              <FormControl fullWidth>
-                <TextField
-                  label="Username"
-                  {...formik.getFieldProps('username')}
-                />
-                {formik.touched.username && formik.errors.username ? (
-                  <FormHelperText error>
-                    {formik.errors.username}
-                  </FormHelperText>
-                ) : null}
-              </FormControl>
-
-              <FormControl fullWidth>
-                <TextField
-                  label="First Name"
-                  {...formik.getFieldProps('firstName')}
-                />
-                {formik.touched.firstName && formik.errors.firstName ? (
-                  <FormHelperText error>
-                    {formik.errors.firstName}
-                  </FormHelperText>
-                ) : null}
-              </FormControl>
-
-              <FormControl fullWidth>
-                <TextField
-                  label="Last Name"
-                  {...formik.getFieldProps('lastName')}
-                />
-                {formik.touched.lastName && formik.errors.lastName ? (
-                  <FormHelperText error>
-                    {formik.errors.lastName}
-                  </FormHelperText>
-                ) : null}
-              </FormControl>
-
-              <FormControl fullWidth>
-                <TextField
-                  label="Password"
-                  {...formik.getFieldProps('password')}
-                />
-                {formik.touched.password && formik.errors.password ? (
-                  <FormHelperText error>
-                    {formik.errors.password}
-                  </FormHelperText>
-                ) : null}
-              </FormControl>
-
-              <FormControl fullWidth>
-                <TextField label="Gender" {...formik.getFieldProps('gender')} />
-                {formik.touched.gender && formik.errors.gender ? (
-                  <FormHelperText error>{formik.errors.gender}</FormHelperText>
-                ) : null}
-              </FormControl>
-
-              <FormControl fullWidth>
-                <TextField
-                  label="Country"
-                  {...formik.getFieldProps('countryOfOrigin')}
-                />
-                {formik.touched.countryOfOrigin &&
-                formik.errors.countryOfOrigin ? (
-                  <FormHelperText error>
-                    {formik.errors.countryOfOrigin}
-                  </FormHelperText>
-                ) : null}
-              </FormControl>
-
-              <FormControl fullWidth>
-                <TextField
-                  label="Current Role"
-                  {...formik.getFieldProps('currentRole')}
-                />
-                {formik.touched.currentRole && formik.errors.currentRole ? (
-                  <FormHelperText error>
-                    {formik.errors.currentRole}
-                  </FormHelperText>
-                ) : null}
-              </FormControl>
-
-              <Button
-                fullWidth
-                type="submit"
-                color="secondary"
-                variant="contained"
-                disabled={isPending} //can't click on the button when loading
+            <div>
+              <div className="flex flex-col items-center">
+                <p className="text-4xl font-extrabold text-[#7B8D6A] mb-2">
+                  BucketMap
+                </p>
+                <p className="text-sm text-gray-600 mb-6">
+                  Explore. Mark. Remember.
+                </p>
+              </div>
+              <form
+                onSubmit={formik.handleSubmit}
+                className="flex flex-col justify-between min-w-[400px] max-w-[600px] items-center shadow-2xl shadow-gray-500 px-8 py-4 min-h-[400px] gap-3"
               >
-                Register
-              </Button>
-              <Link href={'/login'} className="text-blue-700 underline">
-                Already Registered? Login
-              </Link>
-            </form>
+                <p className="text-3xl font-bold">Create a new account</p>
+
+                <FormControl fullWidth>
+                  <TextField label="Email" {...formik.getFieldProps('email')} />
+                  {formik.touched.email && formik.errors.email ? (
+                    <FormHelperText error>{formik.errors.email}</FormHelperText>
+                  ) : null}
+                </FormControl>
+
+                <FormControl fullWidth>
+                  <TextField
+                    label="Username"
+                    {...formik.getFieldProps('username')}
+                  />
+                  {formik.touched.username && formik.errors.username ? (
+                    <FormHelperText error>
+                      {formik.errors.username}
+                    </FormHelperText>
+                  ) : null}
+                </FormControl>
+
+                {/* First Name and Last Name on the same line */}
+                <div className="flex w-full gap-3">
+                  <FormControl fullWidth>
+                    <TextField
+                      label="First Name"
+                      {...formik.getFieldProps('firstName')}
+                    />
+                    {formik.touched.firstName && formik.errors.firstName ? (
+                      <FormHelperText error>
+                        {formik.errors.firstName}
+                      </FormHelperText>
+                    ) : null}
+                  </FormControl>
+                  <FormControl fullWidth>
+                    <TextField
+                      label="Last Name"
+                      {...formik.getFieldProps('lastName')}
+                    />
+                    {formik.touched.lastName && formik.errors.lastName ? (
+                      <FormHelperText error>
+                        {formik.errors.lastName}
+                      </FormHelperText>
+                    ) : null}
+                  </FormControl>
+                </div>
+
+                <FormControl fullWidth variant="outlined">
+                  <InputLabel htmlFor="outlined-adornment-password">
+                    Password
+                  </InputLabel>
+
+                  <OutlinedInput
+                    {...formik.getFieldProps('password')}
+                    // type={showPassword ? 'text' : 'password'}
+                    type={showPassword ? 'text' : 'password'}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label={
+                            showPassword
+                              ? 'hide the password'
+                              : 'display the password'
+                          }
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          onMouseUp={handleMouseUpPassword}
+                          edge="end"
+                        >
+                          {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                    label="Password"
+                  />
+
+                  {formik.touched.password && formik.errors.password ? (
+                    <FormHelperText error>
+                      {formik.errors.password}
+                    </FormHelperText>
+                  ) : null}
+                </FormControl>
+
+                <FormControl fullWidth>
+                  <span className="text-gray-500">Gender</span>
+                  <div className="flex justify-between gap-3">
+                    <TextField
+                      value="Male"
+                      InputProps={{
+                        startAdornment: (
+                          <Radio
+                            value="male"
+                            checked={formik.values.gender === 'male'}
+                            onChange={() =>
+                              formik.setFieldValue('gender', 'male')
+                            }
+                          />
+                        ),
+                        readOnly: true,
+                      }}
+                      onClick={() => formik.setFieldValue('gender', 'male')}
+                    />
+                    <TextField
+                      value="Female"
+                      InputProps={{
+                        startAdornment: (
+                          <Radio
+                            value="female"
+                            checked={formik.values.gender === 'female'}
+                            onChange={() =>
+                              formik.setFieldValue('gender', 'female')
+                            }
+                          />
+                        ),
+                        readOnly: true,
+                      }}
+                      onClick={() => formik.setFieldValue('gender', 'female')}
+                    />
+                    <TextField
+                      value="Other"
+                      InputProps={{
+                        startAdornment: (
+                          <Radio
+                            value="other"
+                            checked={formik.values.gender === 'other'}
+                            onChange={() =>
+                              formik.setFieldValue('gender', 'other')
+                            }
+                          />
+                        ),
+                        readOnly: true,
+                      }}
+                      onClick={() => formik.setFieldValue('gender', 'other')}
+                    />
+                  </div>
+                  {formik.touched.currentRole && formik.errors.currentRole ? (
+                    <FormHelperText error>
+                      {formik.errors.currentRole}
+                    </FormHelperText>
+                  ) : null}
+                </FormControl>
+
+                <FormControl fullWidth>
+                  <InputLabel>Country</InputLabel>
+                  <Select
+                    {...formik.getFieldProps('countryOfOrigin')}
+                    label="Country"
+                  >
+                    {countries.map((item, index) => {
+                      return (
+                        <MenuItem key={index} value={item}>
+                          {item}
+                        </MenuItem>
+                      );
+                    })}
+                  </Select>
+                  {formik.touched.countryOfOrigin &&
+                  formik.errors.countryOfOrigin ? (
+                    <FormHelperText error>
+                      {formik.errors.countryOfOrigin}
+                    </FormHelperText>
+                  ) : null}
+                </FormControl>
+
+                <FormControl fullWidth>
+                  <span className="text-gray-500">Current Role</span>
+                  <div className="flex w-full gap-3">
+                    <TextField
+                      value="Explorer"
+                      InputProps={{
+                        startAdornment: (
+                          <Radio
+                            value="explorer"
+                            checked={formik.values.currentRole === 'explorer'}
+                            onChange={() =>
+                              formik.setFieldValue('currentRole', 'explorer')
+                            }
+                          />
+                        ),
+                        readOnly: true,
+                      }}
+                      onClick={() =>
+                        formik.setFieldValue('currentRole', 'explorer')
+                      }
+                    />
+                    <TextField
+                      value="Editor"
+                      InputProps={{
+                        startAdornment: (
+                          <Radio
+                            value="editor"
+                            checked={formik.values.currentRole === 'editor'}
+                            onChange={() =>
+                              formik.setFieldValue('currentRole', 'editor')
+                            }
+                          />
+                        ),
+                        readOnly: true,
+                      }}
+                      onClick={() =>
+                        formik.setFieldValue('currentRole', 'editor')
+                      }
+                    />
+                  </div>
+                  {formik.touched.currentRole && formik.errors.currentRole ? (
+                    <FormHelperText error>
+                      {formik.errors.currentRole}
+                    </FormHelperText>
+                  ) : null}
+                </FormControl>
+
+                <Button
+                  fullWidth
+                  type="submit"
+                  sx={{ backgroundColor: '#7B8D6A', color: '#fff' }}
+                  variant="contained"
+                  disabled={isPending} //can't click on the button when loading
+                >
+                  Register
+                </Button>
+                <span>
+                  Have an account?{' '}
+                  <Link href={'/login'} className="text-blue-700 font-bold">
+                    Log in
+                  </Link>
+                </span>
+              </form>
+
+              <Snackbar
+                open={snackbarOpen}
+                autoHideDuration={6000}
+                message={snackbarMessage}
+                onClose={() => setSnackbarOpen(false)}
+              />
+            </div>
           );
         }}
       </Formik>
@@ -183,5 +365,3 @@ const Register = () => {
 };
 
 export default Register;
-
-//TODO password visibility, drop down menu
